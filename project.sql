a4a06149cfc1e2cbd6e99dac53c7b4e2845e2284
Emilio Braun <braunemi@raunemi@mail.gvsu.edu>
2018-04-18 18:37:34 -0400

    Finish table creation (Update if needed)

--- project.sql
+++ project.sql
@@ -21,7 +21,7 @@ CONSTRAINT mIC1 PRIMARY KEY (mID),
 
 CONSTRAINT mIC2 CHECK (mType IN ('primary', 'affiliate')),
 
-CONSTRAINT mIC3 CHECK ((mType IS 'primary' AND mFee == 100) || (mType IS'affiliate' AND mFee == 80)), 
+CONSTRAINT mIC3 CHECK ((mType IS 'primary' AND mFee == 100) || (mType IS'affiliate' AND mFee == 80)) 
 );
 ---------------------
 CREATE TABLE Employee (
@@ -41,9 +41,7 @@ CONSTRAINT eIC2 FOREIGN KEY (s_eID) REFERENCES Employee (eID)
 
 CONSTRAINT eIC3 FOREIGN KEY (e_jID) REFERENCES Jobs (jID)
 		ON DELETE SET NULL
-		DEFERRABLE INITIALLY DEFERRED,
-CONSTRAINT eIC4   
-
+		DEFERRABLE INITIALLY DEFERRED
 );
 ---------------------
 CREATE TABLE Jobs (
@@ -53,7 +51,10 @@ j_dID	   INTEGER	NOT NULL,
 --
 CONSTRAINT jIC1 PRIMARY KEY (jID),
 
-CONSTRAINT jIC2 
+CONSTRAINT jIC2 FOREIGN KEY (j_dID) REFERENCES Department (dID)
+		ON DELETE SET NULL
+		DEFERRABLE INITIALLY DEFERRED
+);
 
 );
 ---------------------
@@ -61,10 +62,7 @@ CREATE TABLE Departments (
 dID	   INTEGER,
 dName	   CHAR(15)	NOT NULL,
 --
-CONSTRAINT dIC1 PRIMARY KEY (dID),
-
-CONSTRAINT dIC2
-
+CONSTRAINT dIC1 PRIMARY KEY (dID)
 );
 --------------------	
 CREATE TABLE Products (
@@ -78,58 +76,52 @@ sDate	   CHAR(15)	NOT NULL,
 --
 CONSTRAINT pIC1 PRIMARY KEY (pID),
 
-CONSTRAINT pIC2 
+CONSTRAINT pIC2 FOREIGN KEY (p_dID) REFERENCES Department (dID)
+		ON DELETE SET NULL
+		DEFERRABLE INITIALLY DEFERRED,
 
+CONSTRAINT pIC3 FOREIGN KEY (p_sID) REFERENCES Suppliers (sID)
+		ON DELETE SET NULL
+		DEFERRABLE INITIALLY DEFERRED
 );
 --------------------
 CREATE TABLE Suppliers (
 sID	   INTEGER,
 sName	   CHAR(15)	NOT NULL,
 --
-CONSTRAINT sIC1 PRIMARY KEY (sID),
-
-CONSTRAINT sIC2
-
+CONSTRAINT sIC1 PRIMARY KEY (sID)
 );
 --------------------
 CREATE TABLE Transactions (
 t_mID	   INTEGER,
-tTimestamp INTEGER,
+tTimestamp CHAR(18),
 tTotal	   INTEGER	NOT NULL,
 --
-CONSTRAINT tIC1 PRIMARY KEY (t_mID, tTimestamp),
-
-CONSTRAINT tIC2
-
+CONSTRAINT tIC1 PRIMARY KEY (t_mID, tTimestamp)
 );
 --------------------
 CREATE TABLE mPhone (
 m_mID	   INTEGER,
-mPhone	   CHAR(15),
+mPhone	   CHAR(11),
 --
-CONSTRAINT mpIC1 PRIMARY KEY (m_mID, mPhone),
-
-CONSTRAINT mpIC2
-
+CONSTRAINT mpIC1 PRIMARY KEY (m_mID, mPhone)
 );
 --------------------
 CREATE TABLE ePhone (
 e_eID	   INTEGER,
-ePhone	   CHAR(15),
+ePhone	   CHAR(11),
 --
-CONSTRAINT epIC1 PRIMARY KEY (e_eID, ePhone),
-
-CONSTRAINT epIC2
-
+CONSTRAINT epIC1 PRIMARY KEY (e_eID, ePhone)
 );
 --------------------
 CREATE TABLE prod_Trans (
 pt_mID	   INTEGER,
-pt_tTimestamp CHAR(15),
+pt_tTimestamp CHAR(18),
 pt_pID	   INTEGER,
 --
-CONSTRAINT ptIC1 PRIMARY KEY (pt_mID, pt_tTimestamp, pt_pID),
-
-CONSTRAINT ptIC2
-
+CONSTRAINT ptIC1 PRIMARY KEY (pt_mID, pt_tTimestamp, pt_pID)
 );
+--
+--------------------
+SET FEEDBACK OFF
+
