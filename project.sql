a9d20d508483852ca2e80881627e8aae4f5e350b
Emilio Braun <braunemi@raunemi@mail.gvsu.edu>
2018-04-18 17:52:03 -0400

    Add README file and Creatnd create tables

---
+++ project.sql
@@ -0,0 +1,131 @@
+SPOOL project.out
+SET ECHO ON
+/*
+CIS 353 - Database Design Project
+<Emilio Braun>
+<Edric Lin>
+<Aaron Bager>
+*/
+-- Creating the tables
+- _-------------------
+CREATE TABLE Members (
+mID	   INTEGER,
+mFirst 	   CHAR(15)	NOT NULL,
+mLast 	   CHAR(15)	NOT NULL,
+mAddress   CHAR(30)	NOT NULL,
+mFee	   INTEGER	NOT NULL,
+mType	   CHAR(10)	NOT NULL,
+a_mID	   INTEGER,
+--
+CONSTRAINT mIC1 PRIMARY KEY (mID),
+
+CONSTRAINT mIC2 CHECK (mType IN ('primary', 'affiliate')),
+
+CONSTRAINT mIC3 CHECK ((mType IS 'primary' AND mFee == 100) || (mType IS'affiliate' AND mFee == 80)),
+
+CONSTRAINT mIC4 CHECK 
+
+);
+---------------------
+CREATE TABLE Employee (
+eID	   INTEGER,
+eFirst 	   CHAR(15)	NOT NULL,
+eLast 	   CHAR(15)	NOT NULL,
+eAddress   CHAR(30)	NOT NULL,
+eSalary    INTEGER	NOT NULL,
+s_eID	   INTEGER, 
+e_jID	   INTEGER	NOT NULL,
+--
+CONSTRAINT eIC1 PRIMARY KEY (eID),
+
+CONSTRAINT eIC2 
+
+);
+---------------------
+CREATE TABLE jobs (
+jID	   INTEGER,
+jName	   CHAR(15)	NOT NULL,
+j_dID	   INTEGER	NOT NULL,
+--
+CONSTRAINT jIC1 PRIMARY KEY (jID),
+
+CONSTRAINT jIC2 
+
+);
+---------------------
+CREATE TABLE departments (
+dID	   INTEGER,
+dName	   CHAR(15)	NOT NULL,
+--
+CONSTRAINT dIC1 PRIMARY KEY (dID),
+
+CONSTRAINT dIC2
+
+);
+--------------------	
+CREATE TABLE products (
+pID	   INTEGER,
+pName	   CHAR(15)	NOT NULL,
+pInventory INTEGER	NOT NULL,
+pPrice	   INTEGER	NOT NULL,
+p_dID	   INTEGER	NOT NULL,
+p_sID	   INTEGER	NOT NULL,
+sDate	   CHAR(15)	NOT NULL,
+--
+CONSTRAINT pIC1 PRIMARY KEY (pID),
+
+CONSTRAINT pIC2 
+
+);
+--------------------
+CREATE TABLE suppliers (
+sID	   INTEGER,
+sName	   CHAR(15)	NOT NULL,
+--
+CONSTRAINT sIC1 PRIMARY KEY (sID),
+
+CONSTRAINT sIC2
+
+);
+--------------------
+CREATE TABLE transactions (
+t_mID	   INTEGER,
+tTimestamp INTEGER,
+tTotal	   INTEGER	NOT NULL,
+--
+CONSTRAINT tIC1 PRIMARY KEY (t_mID, tTimestamp),
+
+CONSTRAINT tIC2
+
+);
+--------------------
+CREATE TABLE mPhone (
+m_mID	   INTEGER,
+mPhone	   CHAR(15),
+--
+CONSTRAINT mpIC1 PRIMARY KEY (m_mID, mPhone),
+
+CONSTRAINT mpIC2
+
+);
+--------------------
+CREATE TABLE ePhone (
+e_eID	   INTEGER,
+ePhone	   CHAR(15),
+--
+CONSTRAINT epIC1 PRIMARY KEY (e_eID, ePhone),
+
+CONSTRAINT epIC2
+
+);
+--------------------
+CREATE TABLE prod_Trans (
+pt_mID	   INTEGER,
+pt_tTimestamp CHAR(15),
+pt_pID	   INTEGER,
+--
+CONSTRAINT ptIC1 PRIMARY KEY (pt_mID, pt_tTimestamp, pt_pID),
+
+CONSTRAINT ptIC2
+
+);
